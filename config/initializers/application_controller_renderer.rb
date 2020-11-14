0246431a3ed63cbc0ecf33ddf16a0b2dc3d02350
zawasho <shoozawa20@gmail.com>
2020-11-14 09:35:33 +0000

    [Start] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0b4c35ef2a45a3e07672855fec4cff5f522d0e40d73cb977e04ff761972daeb698773db3d23dafe69d15048c31716dfcefb5135dd79335c8c1ab1ec5be78a986'
